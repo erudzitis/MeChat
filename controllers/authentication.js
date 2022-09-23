487654ea6d8ce8092b3779f73adc0ce895a9d345
Ernests <rudzitisernests1@gmail.com>
2022-09-23 11:22:18 +0200

    login controller

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -44,7 +44,38 @@ const registerController = async (req, res) => {
 
 // [POST] Login controller for users
 const loginController = async (req, res) => {
+    const { username, password } = req.body;
 
+    // Missing parameters
+    if (!username | !password) {
+        throw new customError("Post body parameters missing!", StatusCodes.BAD_REQUEST);
+    }    
+
+    // Checking whether requested user even exists
+    const requestUser = await userModel.query().findOne({
+        username: username
+    });
+
+    // User with provided email already exists
+    if (!requestUser) {
+        throw new customError("User not found!", StatusCodes.NOT_FOUND);
+    } 
+    
+    // Proceed with password validation
+    const validateHash = await argon2.verify(requestUser.password, password);
+
+    // Passwords do not match
+    if (!validateHash) {
+        throw new customError("Passwords do not match!", StatusCodes.UNAUTHORIZED);
+    }
+
+    // Otherwise login was successful, proceed with token generation
+    const token = requestUser.generateJWT();
+
+    res.status(StatusCodes.OK).json({
+        success: true,
+        token: token
+    })
 };
 
 // Exports
