1614915ab8c2f4420355ad126fb19b16204f8b0e
Ernests <rudzitisernests1@gmail.com>
2022-09-25 11:43:43 +0200

    react-redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,25 @@
+// Requirements
 import React from "react";
 import ReactDOM from "react-dom/client";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+
+// Components
 import App from "./App";
+
+// CSS
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = configureStore({ reducer: rootReducer, middleware: [thunk] });
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
