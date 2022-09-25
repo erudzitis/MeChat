e4c6e43fa57d7f509a1ec758323e363e42936d3a
Ernests <rudzitisernests1@gmail.com>
2022-09-26 00:10:51 +0200

    create room changes

---
+++ frontend/src/components/Home/CreateRoomSecondary.jsx
@@ -0,0 +1,41 @@
+// Requirements
+import React, { useState } from "react";
+import { Chips } from "primereact/chips";
+
+// Components
+import SearchInput from "../Main/SearchInput";
+import ChatListBox from "../Main/ChatListBox";
+import InputFloatLabel from "../Main/InputFloatLabel";
+
+const CreateRoomSecondary = ({ setStep }) => {
+    const [chipValues, setChipValues] = useState([]);
+
+    return (
+        <div className="flex flex-column align-items-center justify-content-center gap-5 mt-2">
+            <SearchInput id="search-contacts" size="sm" placeholder="Type contact name" />
+            <Chips
+                placeholder="Selected users..."
+                size="sm"
+                value={chipValues}
+                onChange={(e) => setChipValues(e.value) }
+                onAdd={(value) => console.log("Added value: ", value)}
+                onRemove={(value) => console.log("Removed value: ", value)}
+            />
+
+            <div className="flex flex-column">
+                <ChatListBox username={"Adelle"} chatPreview={"Hey, I am using chat application!"} borderBottom
+                    onClick={() => {
+                        chipValues.indexOf("Adelle") == -1 && setChipValues([...chipValues, "Adelle"])
+                    }}
+                />
+                <ChatListBox username={"Ardis"} chatPreview={"Available"} borderBottom />
+                <ChatListBox username={"Eduards"} chatPreview={"Business, Money, Cars"} borderBottom />
+                <ChatListBox username={"Viliams"} chatPreview={""} borderBottom />
+                <ChatListBox username={"Zigismunds"} chatPreview={"Hey, I am using chat application!"} borderBottom />
+                <ChatListBox username={"Zigvards"} chatPreview={":)"} borderBottom />
+            </div>
+        </div>
+    )
+}
+
+export default CreateRoomSecondary;
