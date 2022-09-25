e46f42e824aa49db088b77288f91bf88b93842d8
Ernests <rudzitisernests1@gmail.com>
2022-09-26 00:10:51 +0200

    create room changes

--- frontend/src/components/Home/CreateRoom.jsx
+++ frontend/src/components/Home/CreateRoom.jsx
@@ -1,29 +1,21 @@
 // Requirements
-import React from "react";
-import { Divider } from "primereact/divider";
+import React, { useState } from "react";
 
 // Components
-import SearchInput from "../Main/SearchInput";
-import ChatListBox from "../Main/ChatListBox";
+import CreateRoomPrimary from "./CreateRoomPrimary";
+import CreateRoomSecondary from "./CreateRoomSecondary";
 
 const CreateRoom = () => {
+    const [componentStep, setComponentStep] = useState(0);
+
+    const componentList = [
+        <CreateRoomPrimary setStep={setComponentStep} />,
+        <CreateRoomSecondary setStep={setComponentStep} />
+    ]    
+
     return (
-        <div className="flex flex-column align-items-center justify-content-center gap-5 mt-2">
-            <SearchInput id="search-contacts" size="sm" placeholder="Search contacts" />
-
-            <div className="flex flex-column">
-                <ChatListBox username={"New Group"} chatPreview={"Click here to create a new group..."} />
-            </div>
-
-            <div className="flex flex-column">
-                <ChatListBox username={"Adelle"} chatPreview={"Hey, I am using chat application!"} borderBottom />
-                <ChatListBox username={"Ardis"} chatPreview={"Available"} borderBottom />
-                <ChatListBox username={"Eduards"} chatPreview={"Business, Money, Cars"} borderBottom />
-                <ChatListBox username={"Viliams"} chatPreview={""} borderBottom />
-                <ChatListBox username={"Zigismunds"} chatPreview={"Hey, I am using chat application!"} borderBottom />
-                <ChatListBox username={"Zigvards"} chatPreview={":)"} borderBottom />
-            </div>
-
+        <div>
+            { componentList[componentStep] }
         </div>
     )
 }
