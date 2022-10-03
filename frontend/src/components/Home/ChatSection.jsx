ba958ff2a05436e40758db04775359d2c769a483
Ernests <rudzitisernests1@gmail.com>
2022-10-03 11:00:10 +0200

    staged changes

--- frontend/src/components/Home/ChatSection.jsx
+++ frontend/src/components/Home/ChatSection.jsx
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import { useSelector, useDispatch } from "react-redux";
+import jwtDecode from "jwt-decode";
 
 // Components
 import AvatarButton from "../Main/AvatarButton";
+import ChatMessage from "../Main/ChatMessage";
 
 // Actions
 import { createMessageAction } from "../../actions/chat";
@@ -14,6 +16,9 @@ const ChatSection = () => {
     const dispatch = useDispatch();
     const { roomData } = useSelector(state => state.chat);
     const [inputMessage, setInputMessage] = useState("");
+
+    // TODO: write a component wrapper that deals with jwt authorization/decoding
+    const decodedUser = jwtDecode(localStorage.getItem("chatApplicationToken"));
 
     const handleChatMessage = () => {
         const query = {
@@ -25,24 +30,29 @@ const ChatSection = () => {
     }
 
     return (
-        <div className="flex flex-1 flex-column border-2 border-red-500">
+        <div className="flex flex-1 flex-column">
             {/* Top wrapper */}
-            <div className="flex align-items-center h-5rem border-2 border-green-500">
+            <div className="flex align-items-center h-5rem">
                 <AvatarButton image={`${process.env.PUBLIC_URL}/images/defaultAvatar.png`} shape="circle" size="xlarge" />
                 <h4 className="p-0 m-0 ml-2 text-white">Chat with user</h4>
             </div>
 
             {/* Middle wrapper */}
-            <div className="flex flex-1 flex-column align-items-center justify-content-center">
+            <div className="flex flex-1 flex-shrink-0 flex-column gap-2 overflow-y-auto p-1 pl-5 pr-5">
                 {roomData?.messages && roomData.messages.map(message => {
                     return (
-                        <h4>{message.content}</h4>
+                        <ChatMessage
+                            key={`chat-message-${roomData.roomId}-${message.id}`}
+                            content={message.content}
+                            timestamp={message.created_at}
+                            myMessage={decodedUser.id === message.user_id}
+                        />
                     )
                 })}
             </div>
 
             {/* Bottom wrapper */}
-            <div className="flex flex-row align-items-center justify-content-center gap-1 h-5rem border-2 border-blue-500 p-2">
+            <div className="flex flex-row align-items-center justify-content-center gap-1 h-5rem p-2">
                 <InputText type="text" className="p-inputtext-md w-full" value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} />
                 <Button onClick={handleChatMessage} label="Send" icon="pi pi-send" iconPos="right" />
             </div>
---
+++ frontend/src/components/Main/ChatMessage.jsx
@@ -0,0 +1,14 @@
+// Requirements
+import React from "react";
+
+const ChatMessage = ({ content, myMessage, timestamp }) => {
+    return (
+        <div className={`flex justify-content-${myMessage ? "end" : "start"}`}>
+            <div className={`flex align-items-center w-max border-round-3xl pl-2 pr-2 ${myMessage ? "bg-primary" : "surface-card"}`}>
+                <h4 className="p-0 font-normal">{content}</h4>
+            </div>
+        </div>
+    )
+}
+
+export default ChatMessage;
