3ad8084e941e59595de1fdbf2454baf89ee349b9
Ernests <rudzitisernests1@gmail.com>
2022-11-10 19:25:42 +0100

    registration multi step form with profile customization

--- frontend/src/components/Authorization/Card.jsx
+++ frontend/src/components/Authorization/Card.jsx
@@ -12,72 +12,67 @@ import Stack from "../Custom/Stack";
 import Center from "../Custom/Center";
 import Link from "../Custom/Link";
 
-const Card = ({ children, type, fn }) => {
-    const { control, handleSubmit, formState: { errors } } = useForm();
+const Card = ({ children, type, onSuccessfulSubmit, submitDisabled = false }) => {
+    const { control, handleSubmit, formState: { errors }, getValues } = useForm();
     const [rememberMe, setRememberMe] = useState(true);
 
     const formState = useSelector(state => state.helper[type]);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    // Function that proceeds with communication with the api
-    const handleAuthorization = (data) => {
-        dispatch(fn(data, navigate));
-    }
-
+    // Required check due to registration multi step process
+    const childrenElements = children.type === React.Fragment ? children.props.children : children;
+    
     return (
-        <Center className="surface-card" expand>
-            <div className="fadein animation-duration-500">
-                <div className="text-center mb-5">
-                    <img src={`${process.env.PUBLIC_URL}/images/primaryIcon.png`} alt="chat-application" height={50} />
-                </div>
+        <div className="fadein animation-duration-500">
+            <div className="text-center mb-5">
+                <img src={`${process.env.PUBLIC_URL}/images/primaryIcon.png`} alt="chat-application" height={50} />
+            </div>
 
-                <form onSubmit={handleSubmit(handleAuthorization)}>
-                    <Stack spacing={5}>
-                        {
-                            Children.map(children, (child) => {
-                                return (
-                                    <>
-                                        <Controller
-                                            name={child.props.id}
-                                            control={control}
-                                            rules={{ required: `${child.props.label} is required!` }}
-                                            render={({ field }) => (
-                                                <child.type
-                                                    {...child.props}
-                                                    invalid={errors[child.props.id]}
-                                                    inputRef={field.ref}
-                                                    value={field.value}
-                                                    onChange={field.onChange}
-                                                />
-                                            )}
-                                        />
+            <form onSubmit={handleSubmit(onSuccessfulSubmit)}>
+                <Stack spacing={5}>
+                    {
+                        Children.map(childrenElements, (child) => {
+                            return (
+                                <>
+                                    <Controller
+                                        name={child.props.id}
+                                        control={control}
+                                        rules={child.props.notRequired ? {} : { required: `${child.props.label} is required!` }}
+                                        render={({ field }) => (
+                                            <child.type
+                                                {...child.props}
+                                                invalid={errors[child.props.id]}
+                                                inputRef={field.ref}
+                                                value={getValues(child.props.id)} 
+                                                onChange={field.onChange}
+                                            />
+                                        )}
+                                    />
 
-                                        <Error errors={errors} target={child.props.id} />
-                                    </>
-                                )
-                            })
-                        }
+                                    <Error errors={errors} target={child.props.id} />
+                                </>
+                            )
+                        })
+                    }
 
-                        <Error errors={formState?.error} />
-                    </Stack>
+                    <Error errors={formState?.error} />
+                </Stack>
 
-                    <div className="mt-4">
-                        <div className="flex align-items-center justify-content-between mb-4">
-                            <div className="flex align-items-center">
-                                <Checkbox id="rememberme" className="mr-2" onChange={() => setRememberMe(!rememberMe)} checked={rememberMe} />
-                                <label htmlFor="rememberme">Remember me</label>
-                            </div>
-
-                            <Link href={type === "REGISTER" ? "/login" : "/register"} label={type === "REGISTER" ? "Already registered?" : "Don't have an account?"} />
+                <div className="mt-4">
+                    <div className="flex align-items-center justify-content-between mb-4">
+                        <div className="flex align-items-center">
+                            <Checkbox id="rememberme" className="mr-2" onChange={() => setRememberMe(!rememberMe)} checked={rememberMe} />
+                            <label htmlFor="rememberme">Remember me</label>
                         </div>
 
-                        <Button label={type === "REGISTER" ? "Register" : "Login"} className="w-full" loading={formState?.loading} type="submit" />
+                        <Link href={type === "REGISTER" ? "/login" : "/register"} label={type === "REGISTER" ? "Already registered?" : "Don't have an account?"} />
                     </div>
-                </form>
-            </div>
-        </Center>
 
+                    <Button label={type === "REGISTER" ? "Register" : "Login"} className="w-full" loading={formState?.loading} type="submit" disabled={submitDisabled} />
+                </div>
+            </form>
+        </div>
     )
 }
 
